31a074f2748e3b0dbb94d20c3768ade358bfff8f
IDaneva <daneva_n@abv.bg>
2023-10-20 12:04:15 +0300

    registered DB models to the admin panel

--- Django_project/base/admin.py
+++ Django_project/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Account, Income, Spends, Customer
 
-# Register your models here.
+admin.site.register(Account)
+admin.site.register(Income)
+admin.site.register(Spends)
+admin.site.register(Customer)
+

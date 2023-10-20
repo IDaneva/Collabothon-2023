2f29e210d6fd273c932c69f63d84d4a71f6be533
IDaneva <daneva_n@abv.bg>
2023-10-20 17:50:10 +0300

    tests

--- Django_project/base/views.py
+++ Django_project/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-from base.models import Account, Income, Spends, Customer
+from base.models import Account, Income, Spends
+from django.db.models import Q
 
 
 def set_account_amount(account):
@@ -25,13 +25,22 @@ def home_page(request):
 
 
 def family_page(request):
+    q = request.GET.get("q") if request.GET.get("q") is not None else ""
+    transactions_i = Income.objects.filter(
+        person__name__icontains=q
+    )
     # customer = Customer.objects.get(name="Kiril Kirilov")
     # account = Account.objects.filter(members=customer)[:1]
     account = Account.objects.get(name="Kirilovi")
     incomes = Income.objects.filter(affected_account=account)
     spending = Spends.objects.filter(affected_account=account)
     account.amount = set_account_amount(account)
-    context = {"account": account, "incomes": incomes, "spending": spending
+    members = account.members
+    context = {"account": account,
+               "incomes": incomes,
+               "spending": spending,
+               "members": members,
+               "transactions_i": transactions_i
                }
     return render(request, "family.html", context)
 

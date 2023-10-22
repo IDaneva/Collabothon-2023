b64ee153ee5d2e5e0593c3332656411cb161a1aa
IDaneva <daneva_n@abv.bg>
2023-10-22 09:53:24 +0300

    added customer registration form

---
+++ djangoProject2/base/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Customer
+
+
+class CustomerRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['name', 'date_of_birth', 'personal_number', 'gender']
+        
